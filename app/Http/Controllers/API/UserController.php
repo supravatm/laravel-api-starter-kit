0b8030d816c34faf025077ff8e1c314146d1692b
Supravat Mondal <6244832+supravatm@users.noreply.github.com>
2025-08-23 17:13:21 +0530

    API Authentication using Sanctum

---
+++ app/Http/Controllers/API/UserController.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use App\Mail\WelcomeEmail;
+use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Facades\Auth;
+
+
+class UserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+
+    public function register(Request $request)
+    {
+        $registerUserData = $request->validate([
+            'name' => 'required|string',
+            'email' => 'required|string|email|unique:users',
+            'password' => 'required|min:8'
+        ]);
+        $user = User::create([
+            'name' => $request->input('name'),
+            'email' => $request->input('email'),
+            'password' => Hash::make($request->input('password')),
+        ]);
+
+        // ... user registration logic ...
+
+        $userEmail = $registerUserData['email'];
+        $userName = $registerUserData['name'];
+        
+        Mail::to($userEmail)->send(new WelcomeEmail($userName));
+
+        return response()->json([
+            'message' => 'User Created ',
+        ]);
+    }
+
+    public function login(Request $request)
+    {
+        $loginUserData = $request->validate([
+            'email' => 'required|string|email',
+            'password' => 'required|min:8'
+        ]);
+        $user = User::where('email', $loginUserData['email'])->first();
+        if (!$user || !Hash::check($loginUserData['password'], $user->password)) {
+            return response()->json([
+                'message' => 'Invalid Credentials'
+            ], 401);
+        }
+        $token = $user->createToken($user->name . '-AuthToken')->plainTextToken;
+        return response()->json([
+            'access_token' => $token,
+        ]);
+    }
+
+    public function logout(){
+        auth()->user()->tokens()->delete();
+    
+        return response()->json([
+          "message"=>"logged out"
+        ]);
+    }
+}
